0e002fcbe18f5818e2e228db9666dccc608873b9
ziyou <314888536@qq.com>
2024-07-04 09:45:21 +0800

    文件上传 & 会话ID

--- dev/llm_server.py
+++ dev/llm_server.py
@@ -1,84 +1,133 @@
 # server.py
 
 from json import dumps, loads
+from typing import Any, Optional
 from fastapi import FastAPI, Response
 from fastapi.responses import JSONResponse, StreamingResponse
 import interpreter
 from pydantic import BaseModel
+from interpreter.core.core import OpenInterpreter
 from utils.prompts import PROMPTS
 from uvicorn import Config, Server
-
-OI = interpreter.OpenInterpreter()
-
-class LLMConfigModel(BaseModel):
-    model: str
-    temperature: float
-    context_window: int
-    max_tokens: int
-    max_output: int
-    api_key: str
+import shutil
 
 class RequestModel(BaseModel):
     prompt: str
-    llm_config: LLMConfigModel
+    files: list[dict]
+    system_prompt: str
+    stop: Optional[list[str]]
+    user: str
+    model_name: str
+    api_key: str
+    conversation_id: str
+    model_parameters: dict[str, Any]
 
-app = FastAPI()
-OI.auto_run = True
-OI.llm.context_window = 32000
-OI.llm.max_tokens=4000
-# interpreter.conversation_filename=
-# interpreter.conversation_history=
-OI.llm.supports_vision=True
-OI.computer.emit_images=True
-OI.system_message = PROMPTS.system_message_analyse
+class OI_server:
+    def __init__(self):
+        # self.interpreter = interpreter
+        self.conversation_id = ''
+        self.app = FastAPI()
+        self.OI_session: dict[str, OpenInterpreter] = {}
 
+    def _OI_instance(self, requset: RequestModel) -> OpenInterpreter:
+        print(f"requset.conversation_id: {requset.conversation_id}")
+        if requset.conversation_id not in self.OI_session:
+            new_OI = OpenInterpreter()
 
+            # default parameters
+            new_OI.llm.temperature = 0.0
+            new_OI.auto_run = True
+            new_OI.llm.context_window = 32000
+            new_OI.llm.max_tokens=4000
+            # interpreter.conversation_filename=
+            # interpreter.conversation_history=
+            new_OI.llm.supports_vision=True
+            new_OI.computer.emit_images=True
+            new_OI.system_message = PROMPTS.system_message_analyse
+            new_OI.llm.model = "gpt-4o"
 
-@app.post("/chat")
-def chat_endpoint(item: RequestModel):
-    llm_config = item.llm_config
-    if llm_config and "model" in llm_config:
-        OI.llm.model = llm_config["model"]
-    if llm_config and "api_key" in llm_config:
-        OI.llm.api_key = llm_config["api_key"]
+            # custom parameters
+            new_OI.llm.model = requset.model_name
+            new_OI.llm.api_key = requset.api_key
+            new_OI.system_message += requset.system_prompt
+            for key, value in requset.model_parameters.items():
+                setattr(new_OI.llm, key, value)
 
-    OI.llm.model = "gpt-4o"
-    result = OI.chat(item.prompt, stream=False, display=False)
+            # save to session
+            self.OI_session[requset.conversation_id] = new_OI
+            self.conversation_id = requset.conversation_id
+        return self.OI_session[requset.conversation_id]
 
-    return JSONResponse(content=result)
+    def run(self):
 
-@app.post("/stream_chat")
-def stream_chat_endpoint(item: RequestModel):
-    llm_config = item.llm_config
-    print(llm_config)
-    if llm_config and "model" in llm_config:
-        OI.llm.model = llm_config["model"]
-    if llm_config and "api_key" in llm_config:
-        OI.llm.api_key = llm_config["api_key"]
+        app = self.app
 
+        @app.post("/chat")
+        def chat_endpoint(item: RequestModel):
+            OI = self._OI_instance(item)
+            
+            result = OI.chat(item.prompt, stream=False, display=False)
 
-    def event_stream():
-        for chunk in OI.chat(item.prompt, display=False, stream=True):
-            print(f"chunk: {chunk}")
-            chunk_json = dumps(chunk)
-            print(f"chunk_json: {chunk_json}")
-            yield f"data: {chunk_json}\r\n"
+            return JSONResponse(content=result)
 
-    return StreamingResponse(event_stream(), media_type="text/event-stream")
+        @app.post("/stream_chat")
+        def stream_chat_endpoint(item: RequestModel):
 
-@app.get("/history")
-def history_endpoint():
-    return OI.messages
+            base_path = '/Users/jiangziyou/github/dify/api/storage/'
 
-@app.get("/reset")
-def reset_endpoint():
-    OI.reset()
-    return OI.messages
+            file_paths = ''
+            file_cnt = len(item.files)
+            for file in item.files:
+                src_path = base_path + file['file_path']
+                dest_path = '/Users/jiangziyou/mnt/data/' + file['sheet_name']
+                shutil.copyfile(src_path, dest_path)
+                file_paths += f'{dest_path} \t'
+            print(f"file_paths: {file_paths}")
 
-@app.get("/test")
-def test_endpoint():
-    OI.chat()
+            if file_cnt == 0:
+                join_prompt = item.prompt
+            else:
+                join_prompt = f'我上传了{file_cnt}个文件，文件地址为: {file_paths}, 请按照下面的要求进行分析: \n{item.prompt}'
+            print(join_prompt)
 
-config = Config(app, host="0.0.0.0", port=8090) 
-server = Server(config)
+            OI = self._OI_instance(item)
+            print(OI)
+            print('OI.messages:', OI.messages)
+
+            def event_stream():
+                for chunk in OI.chat(join_prompt, display=False, stream=True):
+                    print(f"chunk: {chunk}")
+                    chunk_json = dumps(chunk)
+                    print(f"chunk_json: {chunk_json}")
+                    yield f"data: {chunk_json}\r\n"
+
+            return StreamingResponse(event_stream(), media_type="text/event-stream")
+        
+        @app.get("/history")
+        def history_endpoint(conversation_id: str):
+            OI = self.OI_session[conversation_id]
+            if OI is None:
+                return []
+            return OI.messages
+
+        @app.get("/reset")
+        def reset_endpoint(conversation_id: str):
+            OI = self.OI_session[conversation_id]
+            if OI is None:
+                return []
+            OI.reset()
+            return OI.messages
+
+        @app.get("/test")
+        def test_endpoint(conversation_id: str):
+            OI = self.OI_session[conversation_id]
+            if OI is None:
+                return 'OI not found'
+            OI.chat()
+        
+        config = Config(app, host="0.0.0.0", port=8000) 
+        server = Server(config)
+        server.run()
+
+server = OI_server()
 server.run()
